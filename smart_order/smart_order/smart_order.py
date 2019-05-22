d9a3487bc6fdda90e32e76bf47f88c86719003e6
Your Name <you@example.com>
2019-05-22 12:56:50 +0530

    updated code

--- smart_order/smart_order/smart_order.py
+++ smart_order/smart_order/smart_order.py
@@ -7,16 +7,35 @@ from frappe.utils import money_in_words
 def makeMR(doc,method):
 	items = []
 	for d in doc.items:
-		if d.qty > d.actual_qty:
+		if d.qty > d.actual_qty and d.qty < d.total_stock:
 			mr = frappe.get_doc({
 			"doctype": "Material Request", 
-			"material_request_type": "Purchase", 
+			"material_request_type": "Material Transfer", 
 			"schedule_date": doc.transaction_date,
+			"against_sales_order":doc.name,
 			"items": [{
 				"item_code": d.item_code,
 				"qty": d.qty-d.actual_qty,
 				"so_qty": d.qty,
-				"warehouse":d.warehouse
+				"warehouse":d.warehouse,
+				"sales_order":doc.name
+				}]
+			})
+			mr.insert(ignore_permissions=True)
+			mr.save()
+
+		elif d.qty > d.total_stock:
+			mr = frappe.get_doc({
+			"doctype": "Material Request", 
+			"material_request_type": "Purchase", 
+			"schedule_date": doc.transaction_date,
+			"against_sales_order":doc.name,
+			"items": [{
+				"item_code": d.item_code,
+				"qty": d.qty-d.total_stock,
+				"so_qty": d.qty,
+				"warehouse":d.warehouse,
+				"sales_order":doc.name
 				}]
 			})
 			mr.insert(ignore_permissions=True)
